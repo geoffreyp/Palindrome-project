a90166c85e5994539c14c0bf1e5eaca75298ec39
Pruvost Geoffrey <geoffrey.pruvost@hotmail.com>
2018-01-08 16:58:51 +0100

    add clean string

--- app/src/main/java/fr/geoffreypruvost/projetpruvost/MainActivity.java
+++ app/src/main/java/fr/geoffreypruvost/projetpruvost/MainActivity.java
@@ -3,18 +3,34 @@ package fr.geoffreypruvost.projetpruvost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.Normalizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView tvNettoyage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        tvNettoyage = findViewById(R.id.clean_str);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void onClickNettoyer(View v){
+        String clean_str = Normalizer.normalize(editText.getText(), Normalizer.Form.NFD);
+        tvNettoyage.setText(clean_str.replaceAll("[^\\p{ASCII}]", ""));
     }
 }
